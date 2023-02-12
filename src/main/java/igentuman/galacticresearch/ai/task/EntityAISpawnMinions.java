2812baf16ce8bd2c5be6f00dcafbd46eabdec8b6
igentuman <igentuman@gmail.com>
2023-02-12 12:46:54 +0100

    spawn minions for other bosses

--- src/main/java/igentuman/galacticresearch/ai/task/EntityAISpawnMinions.java
+++ src/main/java/igentuman/galacticresearch/ai/task/EntityAISpawnMinions.java
@@ -1,7 +1,12 @@
 package igentuman.galacticresearch.ai.task;
 
 import com.google.common.base.Predicates;
+import micdoodle8.mods.galacticraft.core.entities.EntityEvolvedCreeper;
 import micdoodle8.mods.galacticraft.core.entities.EntityEvolvedSkeleton;
+import micdoodle8.mods.galacticraft.core.entities.EntityEvolvedSpider;
+import micdoodle8.mods.galacticraft.core.entities.EntitySkeletonBoss;
+import micdoodle8.mods.galacticraft.planets.mars.entities.EntityCreeperBoss;
+import micdoodle8.mods.galacticraft.planets.venus.entities.EntitySpiderQueen;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
@@ -62,25 +67,33 @@ public class EntityAISpawnMinions extends EntityAIBase {
 
     private void spawnMobs() {
         for(int i = 0; i < 1 + entity.world.rand.nextInt(2); i++) {
-
-            EntityLiving mob = new EntityEvolvedSkeleton(entity.world);
+            EntityLiving mob = null;
+            if(entity instanceof EntitySkeletonBoss) {
+                mob = new EntityEvolvedSkeleton(entity.world);
+            } else if(entity instanceof EntityCreeperBoss) {
+                mob = new EntityEvolvedCreeper(entity.world);
+            } else if(entity instanceof EntitySpiderQueen) {
+                mob = new EntityEvolvedSpider(entity.world);
+            }
 
             float range = 3F;
             mob.setPosition(entity.posX + 0.5 + Math.random() * range - range / 2, entity.posY + 1, entity.posZ + 0.5 + Math.random() * range - range / 2);
             mob.onInitialSpawn(entity.world.getDifficultyForLocation(new BlockPos(mob)), null);
+            mob.setAttackTarget((EntityLivingBase) closestEntity);
+            mob.forceSpawn = true;
+            EntityAIBase task = null;
+            for(EntityAITasks.EntityAITaskEntry t: mob.targetTasks.taskEntries) {
+                if(t.action instanceof EntityAIHurtByTarget) {
+                    task = t.action;
+                    break;
+                }
+            }
+            if(task != null)
+                mob.targetTasks.removeTask(task);//do not attack each other
+
             if(mob instanceof EntityEvolvedSkeleton) {
                 mob.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, new ItemStack(Items.BOW));
-                mob.setAttackTarget((EntityLivingBase) closestEntity);
-                mob.forceSpawn = true;
-                EntityAIBase task = null;
-                for(EntityAITasks.EntityAITaskEntry t: mob.targetTasks.taskEntries) {
-                    if(t.action instanceof EntityAIHurtByTarget) {
-                        task = t.action;
-                        break;
-                    }
-                }
-                if(task != null)
-                mob.targetTasks.removeTask(task);
+
             }
             closestEntity.world.spawnEntity(mob);
         }
